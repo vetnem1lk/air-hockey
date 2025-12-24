70960accfc554183757e0937f73bde6f39431186
Климентьев Владислав <v.klimentyev@investproject.akb>
2025-12-24 13:34:47 +0700

    added glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    if (!glfwInit()) {
+        std::cerr << "GLFW init failed\n";
+        return -1;
+    }
+
+    std::cout << "GLFW OK\n";
+    glfwTerminate();
+    return 0;
     return 0;
 }
